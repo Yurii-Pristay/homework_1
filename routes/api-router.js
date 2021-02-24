c089f8eaf15f8bd0ecf6364fa3a5a8f4658a9fcc
Volodymyr <frontend.dev4@inspiri.to>
2021-02-24 10:19:29 +0200

    rename singIn to signUserIn and signUp to signUserUp functions

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { signIn, signUp } = require('../controllers/auth-controller');
+const { signUserIn, signUserUp } = require('../controllers/auth-controller');
 const { getMyFeed, registerMyFeed, deleteMyFeed } = require('../controllers/feeds-controller');
 const authCheck = require('../middlewares/auth-check');
 const router = new express.Router();
@@ -7,7 +7,7 @@ const router = new express.Router();
 router.get('/feeds', authCheck, getMyFeed);
 router.post('/registerfeed', authCheck, registerMyFeed);
 router.delete('/deletefeed', authCheck, deleteMyFeed);
-router.post('/signin', signIn);
-router.post('/signup', signUp);
+router.post('/signin', signUserIn);
+router.post('/signup', signUserUp);
 
 module.exports = router;
