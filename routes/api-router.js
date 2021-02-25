50f905ec38274c6acdb0948be426b85b903854c8
RomanSavka-Sir <leon.wolflans11@gmail.com>
2021-02-25 18:02:40 +0200

    change name of file - auth-check.js

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { signIn, signUp } = require('../controllers/auth-controller');
 const { getFeed, registerFeed, deleteFeed } = require('../controllers/feeds-controller');
-const authCheck = require('../middlewares/auth-check');
+const authCheck = require('../middlewares/check-auth');
 const router = new express.Router();
 
 router.get('/feeds', authCheck, getFeed);
